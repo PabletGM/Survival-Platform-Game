b083967a9802767ba57fbccb501dab5b802de625
Pablo <123249399+PabletGM@users.noreply.github.com>
2024-02-13 20:19:12 +0100

    create clase enemyVillain, enemySuperVillain

---
+++ src/enemySuperVillain.cpp
@@ -0,0 +1,43 @@
+#include <SFML/Graphics.hpp>
+
+class enemy
+{
+    public:
+        //constructor por defecto
+        enemy(sf::RenderWindow& window, int posXInit, int posYInit);
+
+
+        void update();
+
+
+        void render(sf::RenderWindow& window);
+
+
+    private:
+
+
+        //metodos privados
+        void MoveSprite();
+        void RotateSprite();
+        void ScaleSprite();
+        void ChangeOriginSprite();
+        void LoadTextureInit(sf::RenderWindow& window);
+        void UpdateSprite(sf::RenderWindow& window);
+
+        //pones todo inicialmente
+        void SetInitialPosition(int posX, int posY);
+
+        //vida
+        int maxLife;
+        int actualLife;
+
+        //sprite enemigo
+        sf::Sprite spriteEnemy;
+        //texture enemigo
+        sf::Texture textureEnemy;
+
+        //posicionInicial
+        int posXInit;
+        int posYInit;
+
+};
