fab9a69c4fa2f949a1fe4f9c23cc65698d7d8b00
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-03-12 19:20:30 +0100

    clase bala creada

---
+++ include/shoot.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <position.h>
+#include <screenSize.h>
+class shoot
+{
+public:
+    //constructor por defecto
+    shoot();
+    shoot(limits limits);
+
+
+    void update();
+
+
+    void render(sf::RenderWindow& window);
+
+
+private:
+
+    //animation Vector parametro, en constructor
+    sf::Vector2f animacionVector;
+
+    //cronometro cambio animacion bala
+    sf::Clock cronometro;
+    sf::Time tiempoDeseado = sf::seconds(0.1f);
+
+    //cronometro cambio de animacion
+    void ChangeAnimationTime();
+    void UpdateAnimation();
+    //dimensiones de cada cuadro de animacion
+    int frameWidth;
+    int frameHeight;
+
+
+    int dirXInit;
+    int dirYInit;
+
+    //metodos privados
+    void MoveSprite();
+    void RotateSprite();
+    void ScaleSprite();
+    void ChangeOriginSprite();
+    void LoadTextureInit();
+    void UpdateSprite(sf::RenderWindow& window);
+
+    //pones todo inicialmente
+    void SetInitialPosition(Position p);
+
+    //change X direction
+    void ChangeXDirection();
+    //change Y direction
+    void ChangeYDirection();
+
+    void CheckLimits();
+
+    void FlipSpriteX();
+
+
+
+
+
+    //sprite enemigo
+    sf::Sprite spriteBala;
+    //texture enemigo
+    sf::Texture textureBala;
+
+    //posicionInicial
+    Position p;
+
+};
---
+++ src/shoot.cpp
@@ -0,0 +1,151 @@
+#include "shoot.h"
+
+
+shoot::shoot() :  dirXInit{ 1 }, dirYInit{ 0 }, animacionVector(0.0f, 0.0f),
+frameWidth{ 100 }, frameHeight{ 80 }
+{
+
+}
+
+shoot::shoot(limits limits) :  dirXInit{ 1 }, dirYInit{ 0 }, animacionVector(0.0f, 0.0f),
+frameWidth{ 100 }, frameHeight{ 80 }
+{
+    
+    //cargas textura inicialmente
+    LoadTextureInit();
+    ScaleSprite();
+    ChangeOriginSprite();
+    
+    //se inicia posicion de enemy
+    SetInitialPosition(p);
+}
+
+//check if the enemy goes out of the limits to change the direction
+void shoot::update()
+{
+    MoveSprite();
+}
+
+void shoot::render(sf::RenderWindow& window)
+{
+    UpdateSprite(window);
+
+}
+
+void shoot::ChangeAnimationTime()
+{
+    sf::Time tiempoTranscurrido = cronometro.getElapsedTime();
+
+    if (tiempoTranscurrido >= tiempoDeseado) {
+        // Se ha alcanzado el tiempo deseado, ejecuta la función y reinicia el cronómetro
+        UpdateAnimation();
+        cronometro.restart();
+    }
+}
+
+void shoot::UpdateAnimation()
+{
+    //hacemos comprobacion de si ha llegado al ultimo sprite de animacion, o si se ha pasado del tamaño de la textura
+    if (animacionVector.x * frameWidth >= textureBala.getSize().x)
+    {
+        //reiniciamos animacion
+        animacionVector.x = 0;
+    }
+    // Define el rectángulo de la animación actual usando animacionVector.x * frameWidth para elegir cuadro animacion
+    //en horizontal, en vertical no hay por eso es 0, y tamaño de un sprite en vertical y horizontal
+    spriteBala.setTextureRect(sf::IntRect(static_cast<int>(animacionVector.x) * frameWidth, 0, frameWidth, frameHeight));
+    //sumamos 1 sprite
+    animacionVector.x++;
+}
+
+void shoot::MoveSprite()
+{
+    // position
+    //sprite.setPosition(sf::Vector2f(10.f, 50.f)); // absolute position
+
+    //move position con dirInit
+
+    p.posX += dirXInit;
+    p.posY += dirYInit;
+    CheckLimits();
+    // offset relative to the current position
+    spriteBala.move(sf::Vector2f(dirXInit, dirYInit));
+
+}
+
+void shoot::RotateSprite()
+{
+    //// rotation
+    //sprite.setRotation(90.f); // absolute angle
+    //sprite.rotate(15.f); // offset relative to the current angle;
+}
+
+void shoot::ScaleSprite()
+{
+    // scale
+    spriteBala.setScale(sf::Vector2f(0.5f, 0.5f)); // absolute scale factor
+    //sprite.scale(sf::Vector2f(1.5f, 3.f)); // factor relative to the current scale
+}
+
+void shoot::ChangeOriginSprite()
+{
+    //origen por defecto es el top-left esquina izquierda
+    spriteBala.setOrigin(sf::Vector2f(75.f, 90.f));
+}
+
+
+void shoot::UpdateSprite(sf::RenderWindow& window)
+{
+    ChangeAnimationTime();
+    window.draw(spriteBala);
+}
+
+void shoot::SetInitialPosition(Position p)
+{
+    spriteBala.move(sf::Vector2f(p.posX, p.posY));
+}
+
+void shoot::ChangeXDirection()
+{
+    //change direction
+    dirXInit *= -1;
+    FlipSpriteX();
+
+}
+
+void shoot::ChangeYDirection()
+{
+    dirYInit *= -1;
+    FlipSpriteX();
+}
+
+void shoot::CheckLimits()
+{
+    ////CHECK LIMITS X size
+    //if (p.posX > limitsEnemy.limitDerecho || (p.posX < limitsEnemy.limitIzquierdo))
+    //{
+    //    //out of limits
+    //    ChangeXDirection();
+    //}
+    ////check limits YSize
+    //if (p.posY > limitsEnemy.limitAbajo || p.posY < limitsEnemy.limitArriba)
+    //{
+    //    //out of limits
+    //    ChangeYDirection();
+    //}
+}
+
+void shoot::FlipSpriteX()
+{
+    spriteBala.scale(-1, 1);
+}
+
+
+void shoot::LoadTextureInit()
+{
+    //carga de imagen del proyecto
+    textureBala.loadFromFile("../sprites/bullet/bullet.png");
+    //le ponemos textura
+    spriteBala.setTexture(textureBala);
+    spriteBala.setTextureRect(sf::IntRect(static_cast<int>(animacionVector.x) * frameWidth, 0, frameWidth, frameHeight));
+}
